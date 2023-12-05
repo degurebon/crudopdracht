3cd3844001c521adb35dc2dc5541f5e2c919e1c8
degurebon <sctyh3@gmail.com>
2023-12-05 11:01:37 +0100

    laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -16,29 +16,35 @@ use App\Http\Controllers\PostController;
 |
 */
 
-// returns the home page with all posts
+// homepage with all posts
 Route::get('/', PostController::class .'@index')->name('posts.index');
-// returns the form for adding a post
-Route::get('/posts/create', PostController::class . '@create')->name('posts.create');
-// adds a post to the database
-Route::post('/posts', PostController::class .'@store')->name('posts.store');
-// returns a page that shows a full post
+// full post page
 Route::get('/posts/{post}', PostController::class .'@show')->name('posts.show');
-// returns the form for editing a post
-Route::get('/posts/{post}/edit', PostController::class .'@edit')->name('posts.edit');
-// updates a post
-Route::put('/posts/{post}', PostController::class .'@update')->name('posts.update');
-// deletes a post
-Route::delete('/posts/{post}', PostController::class .'@destroy')->name('posts.destroy');
 
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
+// need auth
+Route::middleware('auth')->group(function () {
+    // add post form
+    Route::get('/posts/create', PostController::class . '@create')->name('posts.create');
+    // add post to database
+    Route::post('/posts', PostController::class .'@store')->name('posts.store');
+    // edit form
+    Route::get('/posts/{post}/edit', PostController::class .'@edit')->name('posts.edit');
+    // update
+    Route::put('/posts/{post}', PostController::class .'@update')->name('posts.update');
+    // delete
+    Route::delete('/posts/{post}', PostController::class .'@destroy')->name('posts.destroy');
+});
 
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-// });
+// dashboard auth
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// require __DIR__.'/auth.php';
+// profile crud
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
