708839b607d71d48c849063ba3deed59bccd8043
degurebon <sctyh3@gmail.com>
2023-12-05 11:23:20 +0100

    routes

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,31 +15,25 @@ use App\Http\Controllers\PostController;
 |
 */
 
-// homepage with all posts
+// returns the home page with all posts
 Route::get('/', PostController::class .'@index')->name('posts.index');
-// full post page
+// returns the form for adding a post
+Route::get('/posts/create', PostController::class . '@create')->name('posts.create');
+// adds a post to the database
+Route::post('/posts', PostController::class .'@store')->name('posts.store');
+// returns a page that shows a full post
 Route::get('/posts/{post}', PostController::class .'@show')->name('posts.show');
+// returns the form for editing a post
+Route::get('/posts/{post}/edit', PostController::class .'@edit')->name('posts.edit');
+// updates a post
+Route::put('/posts/{post}', PostController::class .'@update')->name('posts.update');
+// deletes a post
+Route::delete('/posts/{post}', PostController::class .'@destroy')->name('posts.destroy');
 
-// need auth
-Route::middleware('auth')->group(function () {
-    // add post form
-    Route::get('/posts/create', PostController::class . '@create')->name('posts.create');
-    // add post to database
-    Route::post('/posts', PostController::class .'@store')->name('posts.store');
-    // edit form
-    Route::get('/posts/{post}/edit', PostController::class .'@edit')->name('posts.edit');
-    // update
-    Route::put('/posts/{post}', PostController::class .'@update')->name('posts.update');
-    // delete
-    Route::delete('/posts/{post}', PostController::class .'@destroy')->name('posts.destroy');
-});
-
-// dashboard auth
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-// profile crud
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
